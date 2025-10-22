7c25dfdcd07c80da376d629cb60178215e0de888
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-10-22 13:20:29 -0400

    moves motor

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by:Mercy
+ * Created on: Oct 2025
+ * This program Moves a motor
 */
 
-basic.showString('Hello, World!')
+// variable
+const servoNumberOne = robotbit.Servos.S1
+
+// start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// move 0 degress
+input.onButtonPressed(Button.A, function() {
+    robotbit.Servo(servoNumberOne,0)
+    basic.showNumber(0)
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+// move 180 degress
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumberOne, 180)
+    basic.showNumber(180)
+    basic.clearScreen()
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
